0a7545ad3c6f559d5c375124e29d5accfda6f11c
Serban Teodorescu <teodores@adobe.com>
2025-01-29 17:01:38 +0200

    use address instead of fqdn

--- cassandra-analytics-core/src/main/java/org/apache/cassandra/spark/data/CassandraDataLayer.java
+++ cassandra-analytics-core/src/main/java/org/apache/cassandra/spark/data/CassandraDataLayer.java
@@ -667,7 +667,7 @@ public class CassandraDataLayer extends PartitionedDataLayer implements StartupV
         Collection<CassandraInstance> instances = ring
                                                   .stream()
                                                   .filter(status -> datacenter == null || datacenter.equalsIgnoreCase(status.datacenter()))
-                                                  .map(status -> new CassandraInstance(status.token(), status.fqdn(), status.datacenter()))
+                                                  .map(status -> new CassandraInstance(status.token(), status.address(), status.datacenter()))
                                                   .collect(Collectors.toList());
 
         return new CassandraRing(partitioner, keyspace, replicationFactor, instances);
