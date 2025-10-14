ed8a30930873b9308353b99ef14e612c26221369
Serban Teodorescu <teodores@adobe.com>
2025-10-14 12:11:40 +0300

    debug msg 1

--- cassandra-analytics-common/src/main/java/org/apache/cassandra/spark/data/partitioner/CassandraRing.java
+++ cassandra-analytics-common/src/main/java/org/apache/cassandra/spark/data/partitioner/CassandraRing.java
@@ -202,11 +202,13 @@ public class CassandraRing implements Serializable
         switch (replicationFactor.getReplicationStrategy())
         {
             case SimpleStrategy:
+                LOGGER.info("Simple Strategy!!!");
                 tokenRangeMap.putAll(RangeUtils.calculateTokenRanges(instances,
                                                                      replicationFactor.getTotalReplicationFactor(),
                                                                      partitioner));
                 break;
             case NetworkTopologyStrategy:
+                LOGGER.info("Network Topology Strategy!!!");
                 for (String dataCenter : dataCenters())
                 {
                     int rf = replicationFactor.getOptions().get(dataCenter);
@@ -220,6 +222,7 @@ public class CassandraRing implements Serializable
                     tokenRangeMap.putAll(RangeUtils.calculateTokenRanges(dcInstances,
                                                                          replicationFactor.getOptions().get(dataCenter),
                                                                          partitioner));
+                    LOGGER.info(tokenRangeMap.toString());
                 }
                 break;
             default:
@@ -229,6 +232,7 @@ public class CassandraRing implements Serializable
         // Calculate token range to replica mapping
         replicas.put(Range.openClosed(partitioner.minToken(), partitioner.maxToken()), Collections.emptyList());
         tokenRangeMap.asMap().forEach((instance, ranges) -> ranges.forEach(range -> addReplica(instance, range, replicas)));
+        LOGGER.info("Replicas: {}", replicas);
     }
 
     public Partitioner partitioner()
